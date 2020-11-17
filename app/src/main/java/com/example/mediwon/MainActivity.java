c1b47a9e9ee3f88e11efc20ed4f8e6f7c8b9cea7
yeawonlee <wwon525@naver.com>
2020-11-17 22:55:54 +0900

    UI - 메인화면 App bar, Navigation Drawer

--- app/src/main/java/com/example/mediwon/MainActivity.java
+++ app/src/main/java/com/example/mediwon/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.mediwon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    //private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //profileImage = findViewById(R.id.profile_image);
+        //Glide.with(this).load(R.drawable.ic_pill_medicine_medicines_medical_icon_131306).into(profileImage);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if(id == R.id.edit) {
+
+        }
+        else if(id == R.id.bookmark) {
+
+        }
+        else if(id == R.id.my_setting) {
+
+        }
+        else if(id == R.id.my_review) {
+
+        }
+        else if(id == R.id.withdrawal){
+            // do_withdrawal();
+            finish();
+        }
+        else if(id == R.id.logout) {
+            //onClickLogout();
+        }
+
+        return true;
     }
 }
